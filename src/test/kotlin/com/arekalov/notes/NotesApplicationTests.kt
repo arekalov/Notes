35f38488cb14e031b6dd42b13b2450cec8a8172a
arekalov <artyom.rekalov@gmail.com>
2025-09-13 19:31:57 +0300

    initial commit

---
+++ src/test/kotlin/com/arekalov/notes/NotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arekalov.notes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
